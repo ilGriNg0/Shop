6048b023f5f7b25d72832a369bdab7d7ba57c1d7
whoz <arabchik122@icloud.com>
2024-09-21 23:22:45 +0300

    Добавилось пара комнад

--- Tretiy/CreateShopListViewModel.cs
+++ Tretiy/CreateShopListViewModel.cs
@@ -1,13 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tretiy.Model;
+using CommunityToolkit.Mvvm.Input;
 namespace Tretiy
 {
-    public class CreateShopListViewModel
+    public class CreateShopListViewModel : INotifyPropertyChanged
     {
+        DataModel Model { get; set; }
+        private ObservableCollection<DataModel> _dataModels;
+        public ObservableCollection<DataModel> DataModels
+        {
+            set
+            {
+               
+                _dataModels = value;
+                OnPropertyChanged("DataModels");
+            }
 
+            get { return _dataModels; }
+        }
+        public CreateShopListViewModel()
+        {
+            DataModels = new();
+            DataModels.Add(new());
+        }
+        private RelayCommand _addNewItemInfoPanel;
+
+        public RelayCommand AddNewItemInfoPanel
+        {
+            get { return _addNewItemInfoPanel ?? (_addNewItemInfoPanel ??= new RelayCommand(() =>
+            
+            {           
+                DataModels.Add(new DataModel()); 
+            })); }
+        }
+
+        private RelayCommand _saveDataInfoPanel;
+        public RelayCommand SaveDataInfoPanel
+        {
+            get => _addNewItemInfoPanel ?? (_addNewItemInfoPanel = new RelayCommand(() =>
+            {
+                var item  = DataModels.IndexOf(Model);
+                foreach (var items in DataModels.Skip(item))
+                {
+                    items.NameItem = Model.NameItem;
+                }
+            }));
+        }
+
+       
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
---
+++ Tretiy/Model/DataModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tretiy.Model
+{
+    public class DataModel : INotifyPropertyChanged
+    {
+        private string _nameItem;
+        public string NameItem
+        {
+            set
+            {
+                _nameItem = value;
+                OnPropertyChanged("NameItem");
+            }
+            get => _nameItem;
+        }
+
+        private int _countItem;
+        public int CountItem
+        {
+            set
+            {
+                if(_countItem > 0)
+                {
+                    _countItem = value;
+                    OnPropertyChanged("CountItem");
+                }
+            }
+            get => _countItem;
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public DataModel() { }
+    }
+}
