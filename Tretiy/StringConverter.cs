cf023fb770eb6dd4b64fd98160ed7079821ade9b
ilGriNg0 <93194921+ilGriNg0@users.noreply.github.com>
2024-09-24 19:20:10 +0300

    TextBox upd

---
+++ Tretiy/StringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Tretiy
+{
+    public class StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+          if(value is string str)
+            {
+               
+               return !string.IsNullOrEmpty(str);
+            }
+
+           return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        private bool ZeroValue(string str) => int.TryParse(str, out int val) ? true : false;
+    }
+}
---
+++ Tretiy/TextBoxZeroValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Navigation;
+
+namespace Tretiy
+{
+    public class TextBoxZeroValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int intValue && intValue == 0)
+            {
+                return string.Empty;
+            }
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value?.ToString()))
+            {
+                return 0;
+            }
+            return int.Parse(value.ToString());
+        }
+
+       
+    }
+}
