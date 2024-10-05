0d7c79ce9c1674bf8ed064966e576b32a0a1be63
whoz <arabchik122@icloud.com>
2024-10-05 20:18:56 +0300

    подкинул сервис на watermark

---
+++ Tretiy/WaterMarkServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tretiy
+{
+   public static class WaterMarkServices
+    {
+        public static readonly DependencyProperty WaterMarkProperty = DependencyProperty.RegisterAttached(
+                "WaterMark",
+                typeof(string),
+                typeof(WaterMarkServices),
+                new PropertyMetadata(string.Empty));
+
+        public static void SetWaterMark(UIElement element, string watermark) => element.SetValue(WaterMarkProperty, watermark);
+       
+        public static string GetWaterMark(UIElement element) => (string)element.GetValue(WaterMarkProperty);
+    }
+}
